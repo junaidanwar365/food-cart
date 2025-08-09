80ff13b84426b241c8871920324f9c1d923f00bc
junaidanwar365 <junaidanwar365@gmail.com>
2025-08-09 14:50:40 +0500

    init

---
+++ config.example.php
@@ -0,0 +1,6 @@
+<?php
+$host = "sqlXXX.infinityfree.com"; // Host (find in MySQL panel)
+$dbname = "your_db_name";  // Username
+$username = "your_username"; // Password
+$password = "your_password"; // Database name
+?>
